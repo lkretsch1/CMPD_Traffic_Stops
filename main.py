12b4a820145e4827117971c825d8af718cdd941a
jcksnstvns <jacksonstevens13@gmail.com>
2024-04-08 19:59:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #Import Packages
-import streamlit as st 
-import pandas as pd 
+import streamlit as st
+import pandas as pd
+import altair as alt
 
-st.write('Jackson Stevens was here!')
+@st.cache_data
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+stops = load_data('data/Officer_Traffic_Stops.csv')
 
-st.header("CMPD Traffic Stops")
+## Box plot
+box = alt.Chart(stops).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
 
-stops = pd.read_csv("data/Officer_Traffic_Stops.csv")
-
-st.dataframe(stops)
+st.altair_chart(box)
